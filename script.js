d0045c4b6dab41fde4d1df333d7d7599f7133c04
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-08-20 16:42:00 +0100

    etchasketch project iteration

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // Etch a Sketch JS file
+
+const container = document.getElementById('container');
+const containerSize = 16 * 16;
+
+for (let i = 0; i < containerSize; i++) {
+    const containerItem = document.createElement('div');
+    containerItem.classList.add('container-item');
+    container.appendChild(containerItem);
+}
+
+const containerItem = document.getElementsByClassName('container-item');
+
+for (let item of containerItem) {
+    item.addEventListener('mouseover', function() { 
+        item.style.backgroundColor = "black";
+    });
+}
+
+const btn = document.getElementById('button');
+btn.addEventListener('click', () => {
+    userInput = prompt('Pick how many squares per side you want to generate? Max: 100');
+    if (userInput < 1 || userInput > 100 || isNaN(userInput) || !Number.isInteger(Number(userInput))) {
+        alert('Pick a valid number');
+    } else {
+        containerSize = userInput;
+    }
+})

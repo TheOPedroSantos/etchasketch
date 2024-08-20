1eaed597f4b33dc9459598ba4cd00e6010d1ae1b
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-08-20 17:07:57 +0100

    iteration of the script

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 // Etch a Sketch JS file
 
 const container = document.getElementById('container');
-const containerSize = 16 * 16;
 
-for (let i = 0; i < containerSize; i++) {
-    const containerItem = document.createElement('div');
-    containerItem.classList.add('container-item');
-    container.appendChild(containerItem);
+// Function to create a new grid with 16*16
+function createGrid(size) {
+    container.innerHTML = '';
+    const containerSize = size * size;
+
+    for (let i = 0; i < containerSize; i++) {
+        const containerItem = document.createElement('div');
+        containerItem.classList.add('container-item');
+        container.appendChild(containerItem);
+    }
+
+    const containerItem = document.getElementsByClassName('container-item');
+
+    for (let item of containerItem) {
+        item.addEventListener('mouseover', function() { 
+            item.style.backgroundColor = "black";
+        });
+    }
 }
+createGrid(16);
 
-const containerItem = document.getElementsByClassName('container-item');
-
-for (let item of containerItem) {
-    item.addEventListener('mouseover', function() { 
-        item.style.backgroundColor = "black";
-    });
-}
-
+// Function to create a grid base on user input
 const btn = document.getElementById('button');
 btn.addEventListener('click', () => {
     userInput = prompt('Pick how many squares per side you want to generate? Max: 100');
     if (userInput < 1 || userInput > 100 || isNaN(userInput) || !Number.isInteger(Number(userInput))) {
         alert('Pick a valid number');
     } else {
-        containerSize = userInput;
+        createGrid(userInput);
     }
-})
+});

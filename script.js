9ec6d96ff6bd17023788f78ef9579d3692aad189
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-08-23 14:25:58 +0100

    etchasketch iteration

--- script.js
+++ script.js
@@ -5,27 +5,35 @@ const container = document.getElementById('container');
 // Function to create a new grid with 16*16
 function createGrid(size) {
     container.innerHTML = '';
-    
-    // const squareSize = 820 / size;
-    // container.style.width = `${820}px`;
-    // container.style.height = `${820}px`;
-    // const containerSize = size * size;
 
     container.style.setProperty('--grid-size', size)
 
     for (let i = 0; i < size * size; i++) {
         const containerItem = document.createElement('div');
         containerItem.classList.add('container-item');
-        // containerItem.style.width = `${squareSize}px`;
-        // containerItem.style.height = `${squareSize}px`;
         container.appendChild(containerItem);
     }
 
     const containerItem = document.getElementsByClassName('container-item');
 
+        // Get a random value for the 'limit'
+        const getRandomNumber = (limit) => {
+            return Math.floor(Math.random() * limit);
+        };
+
+        // Get a random background color for the containerItems(squares)
+        const getRandomColor = () => {
+            const h = getRandomNumber(360);
+            const s = getRandomNumber(100);
+            const l = getRandomNumber(100);
+            return `hsl(${h}deg, ${s}%, ${l}%)`;
+        }
+
+    // When mouseover, the background color of the containerItems is changed
     for (let item of containerItem) {
         item.addEventListener('mouseover', function() { 
-            item.style.backgroundColor = "black";
+            const randomColor = getRandomColor();
+            item.style.backgroundColor = randomColor;
         });
     }
 }
